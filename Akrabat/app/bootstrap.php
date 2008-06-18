d962614b7e29ec8e5f012c72178c40d310c46d4d
David Grudl <david@grudl.com>
2008-06-18 02:31:01 +0000

    updated directory structure in examples

--- Akrabat/app/bootstrap.php
+++ Akrabat/app/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
+/*use Nette::Environment;*/
+/*use Nette::Application::Route;*/
 
 /**
  * Load Nette
@@ -7,29 +9,47 @@
 require_once dirname(__FILE__) . '/../../../Nette/loader.php';
 
 
+
+/**
+ * Setup Nette::Debug
+ */
+/*Nette::*/Debug::enable(E_ALL | E_STRICT);
+
+
+
 /**
  * Configure application
  */
 Environment::loadConfig();
 
+if (!is_writable(Environment::getVariable('tempDir'))) {
+	throw new Exception("Make directory '" . Environment::getVariable('tempDir') . "' writable!");
+}
+
+
 
 /**
- * Prepare & setup
+ * Setup database connection
  */
-Debug::enable(E_ALL | E_STRICT);
-
 require_once 'dibi.compact.php';
 dibi::connect(Environment::getConfig('database'));
 
 
+
+/**
+ * Setup router
+ */
 $application = Environment::getApplication();
+
 $router = $application->getRouter();
+
 $router[] = new Route('index.php', array(
-	'presenter' => 'default',
+	'presenter' => 'Default',
+	'view' => 'default',
 ), Route::ONE_WAY);
 
 $router[] = new Route('<presenter>/<view>/<id>', array(
-	'presenter' => 'default',
+	'presenter' => 'Default',
 	'view' => 'default',
 	'id' => NULL,
 ));
--- Modules-Usage/app/bootstrap.php
+++ Modules-Usage/app/bootstrap.php
@@ -1,10 +1,20 @@
 <?php
 
+/*use Nette::Environment;*/
+/*use Nette::Application::Route;*/
 
 /**
  * Load Nette
  */
 require_once dirname(__FILE__) . '/../../../Nette/loader.php';
+
+
+
+/**
+ * Setup Nette::Debug
+ */
+/*Nette::*/Debug::enable(E_ALL | E_STRICT);
+
 
 
 /**
@@ -13,21 +23,21 @@ require_once dirname(__FILE__) . '/../../../Nette/loader.php';
 Environment::loadConfig();
 
 
+
 /**
- * Prepare & setup
+ * Setup router
  */
-Debug::enable(E_ALL | E_STRICT);
-
-
 $application = Environment::getApplication();
+
 $router = $application->getRouter();
+
 $router[] = new Route('index.php', array(
-	'module' => 'front',
-	'presenter' => 'default',
+	'module' => 'Front',
+	'presenter' => 'Default',
 ), Route::ONE_WAY);
 
-$router[] = new Route('<presenter #d>/<view>/<id>', array(
-	'presenter' => 'default',
+$router[] = new Route('<presenter>/<view>/<id>', array(
+	'presenter' => 'Front:Default',
 	'view' => 'default',
 	'id' => NULL,
 ));
