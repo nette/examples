87b21891dd9eb028406b4f6362a8509c28f8b2e3
David Grudl <david@grudl.com>
2014-04-03 19:56:20 +0200

    /vendor is now ignored by GIT
    Conflicts:
    	CD-collection/composer.json
    	Fifteen/composer.json
    	Modules-Usage/composer.json

--- CD-collection/vendor/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// This is default autoload.php. It can be overwritten by Composer.
-
-if (!is_file(__DIR__ . '/../../../Nette/loader.php')) {
-	die("Nette Framework is expected in directory '" . __DIR__ . "/Nette' but not found. Edit file '" . __FILE__ . "' or execute `composer update`.");
-}
-
-require __DIR__ . '/../../../Nette/loader.php';
