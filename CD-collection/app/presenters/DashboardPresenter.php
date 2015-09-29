b9598b02c1ab430e572d1203b81653ae739e19c8
David Grudl <david@grudl.com>
2015-09-29 21:31:22 +0200

    typo

--- CD-collection/app/presenters/DashboardPresenter.php
+++ CD-collection/app/presenters/DashboardPresenter.php
@@ -24,7 +24,7 @@ class DashboardPresenter extends Nette\Application\UI\Presenter
 		parent::startup();
 
 		if (!$this->getUser()->isLoggedIn()) {
-			if ($this->getUser()->logoutReason === Nette\Security\IUserStorage::INACTIVITY) {
+			if ($this->getUser()->getLogoutReason() === Nette\Security\IUserStorage::INACTIVITY) {
 				$this->flashMessage('You have been signed out due to inactivity. Please sign in again.');
 			}
 			$this->redirect('Sign:in', array('backlink' => $this->storeRequest()));
