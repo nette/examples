cd48bd6769e39bdaf6c2745dd3b7bd94df361fef
David Grudl <david@grudl.com>
2018-07-28 13:45:13 +0200

    cs

--- Fifteen/app/components/FifteenControl.php
+++ Fifteen/app/components/FifteenControl.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 use Nette\Application\UI;
@@ -26,7 +27,6 @@ class FifteenControl extends UI\Control
 
 	public function __construct()
 	{
-		parent::__construct();
 		$this->order = range(0, $this->width * $this->width - 1);
 	}
 
--- Fifteen/www/index.php
+++ Fifteen/www/index.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 // let bootstrap create Dependency Injection container
